e46ff0917c4cd1ab42533278c9d39544c8f3d04b
jchorda22@gmail.com <jchorda22@gmail.com>
2018-08-25 01:04:28 +0200

    Solutions.

--- biblioteca_sistedes/static/biblioteca_sistedes/js/better-multiselect-filter.js
+++ biblioteca_sistedes/static/biblioteca_sistedes/js/better-multiselect-filter.js
@@ -113,7 +113,7 @@ function search_table() {
   table = document.getElementById("myTable");
   tr = table.getElementsByTagName("tr");
   for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[1];
+    td = tr[i].getElementsByTagName("td")[0];
     if (td) {
       if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
         tr[i].style.display = "";
